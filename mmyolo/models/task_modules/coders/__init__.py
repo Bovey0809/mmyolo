e0aaac0bd992b9f47213ab813edb98b592737852
houbowei <houowei@gddi.com.cn>
2023-02-13 20:30:10 +0800

    DONE: oks

--- mmyolo/models/task_modules/coders/__init__.py
+++ mmyolo/models/task_modules/coders/__init__.py
@@ -2,9 +2,9 @@
 from .distance_angle_point_coder import DistanceAnglePointCoder
 from .distance_point_bbox_coder import DistancePointBBoxCoder
 from .yolov5_bbox_coder import YOLOv5BBoxCoder
-from .yolox_bbox_coder import YOLOXBBoxCoder
+from .yolox_bbox_coder import YOLOXBBoxCoder, YOLOXKptCoder
 
 __all__ = [
-    'YOLOv5BBoxCoder', 'YOLOXBBoxCoder', 'DistancePointBBoxCoder',
+    'YOLOv5BBoxCoder', 'YOLOXBBoxCoder', 'DistancePointBBoxCoder', 'YOLOXKptCoder',
     'DistanceAnglePointCoder'
 ]
