c0015dfec18e07c9242edaa97c2e0efee38da771
houbowei <houbowei@gddi.com.cn>
2023-02-24 17:50:05 +0800

    DONE: add preprocess

--- mmyolo/models/data_preprocessors/__init__.py
+++ mmyolo/models/data_preprocessors/__init__.py
@@ -1,10 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .data_preprocessor import (PPYOLOEBatchRandomResize,
                                 PPYOLOEDetDataPreprocessor,
-                                YOLOv5DetDataPreprocessor,
-                                YOLOXBatchSyncRandomResize)
+                                YOLOv5DetDataPreprocessor)
 
 __all__ = [
     'YOLOv5DetDataPreprocessor', 'PPYOLOEDetDataPreprocessor',
-    'PPYOLOEBatchRandomResize', 'YOLOXBatchSyncRandomResize'
+    'PPYOLOEBatchRandomResize', 'YOLOXBatchSyncRandomResize', 'YOLOXPoseBatchSyncRandomResize'
 ]
